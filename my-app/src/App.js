a0252fb6c629de45e14543d6c2dd72814792d9e8
MatiasTong <58445480+MatiasTong@users.noreply.github.com>
2021-04-24 11:01:53 -0400

    added simple sorting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 import './App.css'
 import axios from "axios"
 import ButtonAppBar from './components/ButtonAppBar'
@@ -8,29 +8,39 @@ import Item from "./components/Item"
 const token = process.env.REACT_APP_TOKEN
 
 function App() {
-const [data, setData] = useState()
+  const [data, setData] = useState()
 
-  useEffect( async () => {
+  useEffect(async () => {
     const result = await axios.get(
-      "https://api.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=100",
+      "https://api.ebay.com/buy/browse/v1/item_summary/search?q=cars&limit=100",
       {
         headers: {
           "Authorization": `Bearer ${token}`
         }
       }
-      ).catch(error => console.log(error) )
-    
-      setData(result.data.itemSummaries);
-      console.log(data)
+    ).catch(error => console.log(error))
+
+    setData(result.data.itemSummaries);
+    console.log(data)
     // console.log(result);
   }, [])
+
+  const customSort = () => {
+    let array = [...data]
+    array.sort((a, b)=> a.title.toLowerCase() > b.title.toLowerCase() ? 1: -1)
+    setData(array)
+    console.log(data)
+    console.log("new" + JSON.stringify(data.map( (item)=> { return {title:item.title}} )))
+  }
+
 
   return (
     <div className="App">
       <ButtonAppBar />
       {data && data.map((item) =>
-      <Item title = {item.title}/>
+        <Item title={item.title} />
       )}
+      <button onClick={customSort}> Sort </button>
     </div>
   );
 }
