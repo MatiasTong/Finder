aef5f9658ba0ac8bf0bbf8b364b5d79c930d7399
MatiasTong <58445480+MatiasTong@users.noreply.github.com>
2021-04-24 01:05:59 -0400

    added api call method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react"
+import './App.css'
+import axios from "axios"
 import ButtonAppBar from './components/ButtonAppBar'
+import Item from "./components/Item"
+
+const token = process.env.REACT_APP_TOKEN
 
 function App() {
+const [data, setData] = useState()
+
+  useEffect( async () => {
+    const result = await axios.get(
+      "https://api.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=100",
+      {
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      }
+      ).catch(error => console.log(error) )
+    
+      setData(result.data.itemSummaries);
+      console.log(data)
+    // console.log(result);
+  }, [])
+
   return (
     <div className="App">
       <ButtonAppBar />
+      {data && data.map((item) =>
+      <Item title = {item.title}/>
+      )}
     </div>
   );
 }
---
+++ my-app/src/components/Item.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Item({title}) {
+    return (
+        <div style ={{border: "1px solid black"}}>
+            <p>{title}</p>
+        </div>
+    )
+}
